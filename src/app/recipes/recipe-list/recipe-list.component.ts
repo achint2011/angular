8d4f9092f071270f57e745b4c14b5845c0da685d
achint <trivediachint@gmail.com>
2023-02-03 13:05:57 +0530

    Added navigation bar and created recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Poha','A healthy snack in Indian cuisine','https://www.sharmispassions.com/wp-content/uploads/2022/01/PohaRecipe5-500x500.jpg'),
+    new Recipe('Poha','A healthy snack in Indian cuisine','https://www.sharmispassions.com/wp-content/uploads/2022/01/PohaRecipe5-500x500.jpg')
+  ];
 
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+
+  }
 }
