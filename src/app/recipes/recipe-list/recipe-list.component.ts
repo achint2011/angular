07b10f6b8c85135ca5c2b96361ab67634071c9bd
achint <trivediachint@gmail.com>
2023-02-08 11:18:43 +0530

    Added selecting option for Recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Output,EventEmitter, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,16 +6,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Poha','A healthy snack in Indian cuisine','https://www.sharmispassions.com/wp-content/uploads/2022/01/PohaRecipe5-500x500.jpg'),
-    new Recipe('Poha','A healthy snack in Indian cuisine','https://www.sharmispassions.com/wp-content/uploads/2022/01/PohaRecipe5-500x500.jpg')
+    new Recipe('Idli','A healthy snack made with Rice','https://i0.wp.com/gomathirecipes.com/wp-content/uploads/2022/05/292.jpg?fit=1148%2C1077&ssl=1')
   ];
 
 
   constructor(){
 
   }
-  ngOnInit(): void {
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
+  ngOnInit(){
 
   }
 }
