b4afc5822f8cf6a0e6447d3df8b180bc80614be0
achint <trivediachint@gmail.com>
2023-02-09 10:34:31 +0530

    Used renderer for better attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { BasicHighlightDirective } from './basic-highlight/basic-highlight.direc
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    BetterHighlightDirective,
   ],
   imports: [
     BrowserModule,
