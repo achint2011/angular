83122c2d21ec9610cad7735003b798193dd7bf68
achint <trivediachint@gmail.com>
2023-02-15 15:48:09 +0530

    Passing ingredients from recipes to shopping list using service

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component,Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
+import { RecipeService } from '../recipe.service';
+
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
@@ -8,4 +10,10 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent {
   @Input() recipe : Recipe;
+  constructor(private recipeService: RecipeService){
+
+  }
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
