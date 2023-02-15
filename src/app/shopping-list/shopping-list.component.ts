45776d08c6140cbff5f57528490512356d41e935
achint <trivediachint@gmail.com>
2023-02-15 09:24:38 +0530

    Using service for pushing data into shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -16,5 +16,8 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit(){
     this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged.subscribe((ingredients:Ingredient[])=>{
+      this.ingredients=ingredients;
+    })
   }
 }
