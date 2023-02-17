775e1b8e8984a672eb4ad79443bacf585117a1c8
achint <trivediachint@gmail.com>
2023-02-17 11:16:36 +0530

    Fetching Route Parameters

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -30,6 +30,7 @@
 // }
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router,ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -39,10 +40,17 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private router:Router, private route:ActivatedRoute)
+    {
+
+    }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+  onReload(){
+    this.router.navigate(['servers'],{relativeTo: this.route});
   }
 
 }
