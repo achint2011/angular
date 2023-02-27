4624e2be09d30d54a5bbf5be0b13c3ee2f66b400
achint <trivediachint@gmail.com>
2023-02-27 14:38:24 +0530

    Made Arrays of form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@
 // export class AppComponent {
 // }
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -20,15 +20,20 @@ export class AppComponent implements OnInit {
   signupForm: FormGroup;
   ngOnInit(){
     this.signupForm = new FormGroup({
-      'userdata': new FormGroup({
+      'userData': new FormGroup({
       'username': new FormControl(null,Validators.required),
       'email': new FormControl(null,[Validators.required,Validators.email])
       }),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     })
   }
   onSubmit(){
     console.log(this.signupForm);
+  }
+  onAddHobby(){
+    const control = new FormControl(null,Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 }
 
