1a4841902fb4840569cf0b1bee5bf2ade0e2b424
achint <trivediachint@gmail.com>
2023-02-27 11:53:02 +0530

    Using form data and completed template driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,24 +16,51 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signupForm: NgForm;
-  defaultQuestion = 'pet';
+  @ViewChild('f', { static: false }) signupForm: NgForm;
+  defaultQuestion = 'teacher';
   answer = '';
-  genders = ['male','female'];
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
     this.signupForm.form.patchValue({
       userData: {
         username: suggestedName
       }
     });
   }
-  // onSubmit(form: NgForm){
+
+  // onSubmit(form: NgForm) {
   //   console.log(form);
   // }
-  onSubmit(){
-    console.log(this.signupForm);
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
+
 
